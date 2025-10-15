85160273165d225b2bdfeb3dbacfd96438287e0d
witty._0 <mercury0502@dgu.ac.kr>
2025-10-15 21:58:33 +0900

    [Feature] community 작성 페이지 퍼블리싱
    - community 작성 페이지 퍼블리싱
    - community 상세 조회 페이지 퍼블리싱
    - community 작성 수정 페이지 퍼블리싱
    - route 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import Layout from '@/layouts';
 import Community from '@/pages/community';
+import CommunityDetail from '@/pages/communityDetail';
+import CommunityEdit from '@/pages/communityEdit';
+import CommunityWrite from '@/pages/communityWrite';
 import Evaluate from '@/pages/evaluate';
 import EvaluateStart from '@/pages/evaluateStart';
 import Home from '@/pages/home';
@@ -17,6 +20,9 @@ export const router = createBrowserRouter([
       { path: 'myPage', element: <MyPage /> },
       { path: 'evaluate', element: <Evaluate /> },
       { path: 'evaluate/start', element: <EvaluateStart /> },
+      { path: 'community/write', element: <CommunityWrite /> },
+      { path: 'community/edit', element: <CommunityEdit /> },
+      { path: 'community/detail/:id', element: <CommunityDetail /> },
     ],
   },
 ]);
